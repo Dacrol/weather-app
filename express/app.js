f4c877fc668fe3f93a362468ddbc9342d5d4eff0
Dacrol <jonas.ahrens@gmail.com>
2018-10-22 10:29:10 +0200

    Format

--- express/app.js
+++ express/app.js
@@ -1,38 +1,34 @@
-const fetch = require('node-fetch');
+const fetch = require('node-fetch')
 /* 
   The express app is a global called app
   It can respond on all routes under /api
 */
-let app = global.expressApp;
+let app = global.expressApp
 
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.get('/search-location-weather', (req, res) => {
-  
-  const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+  const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?zip='
 
-  const apiId = '&appid=8786c6ec53b7543aec64b953327991ec&units=metric';
+  const apiId = '&appid=8786c6ec53b7543aec64b953327991ec&units=metric'
 
   const userLocation = (url1, url2, zipcode) => {
-    let newUrl = url1 + zipcode + url2;
-    return newUrl;
-  };
+    let newUrl = url1 + zipcode + url2
+    return newUrl
+  }
 
-  const apiUrl = userLocation(baseUrl, apiId, zipcode);
+  const apiUrl = userLocation(baseUrl, apiId, zipcode)
 
   fetch(apiUrl)
-   .then(res => res.json())
-   .then(data => {
-      res.send({ data });
-   })
-   .catch(err => {
-      res.redirect('/error');
-   });
-});  
-
+    .then(res => res.json())
+    .then(data => {
+      res.send({ data })
+    })
+    .catch(err => {
+      res.redirect('/error')
+    })
+})
 
 // Answer hello world on all routes
 app.all('*', (req, res) => {
@@ -40,5 +36,5 @@ app.all('*', (req, res) => {
     message: 'Hello world!',
     method: req.method,
     url: req.url
-  });
-});
+  })
+})
