5fa711bdd11d879c53ca33c8c680b7a372f661c8
Dacrol <jonas.ahrens@gmail.com>
2018-11-04 15:01:31 +0100

    Add data fetching methods

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,26 @@
 import './Home.scss'
 
-@observer export default class Home extends Component {
+@observer
+export default class Home extends Component {
+  @observable
+  city = ''
+  @observable
+  weatherData = {}
 
-  @observable city = ''
   async start() {}
+
+  search(e) {
+    e.preventDefault()
+    fetch('/api/search-weather-city?city=' + this.city, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => res.json())
+      .then(data => {
+        this.weatherData = data
+        console.log(toJS(this.weatherData))
+      })
+  }
 }
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,19 +1,24 @@
 <div className="pt-5 w-100 d-flex justify-content-center align-items-center">
-<div className="text-center">
-  <Form>
-    <FormGroup>
-      <Label for="city">Search weather in:</Label>
-      <Input
-        type="text"
-        name="city"
-        id="city"
-        placeholder="City"
-        value={this.city}
-        onChange={e => this.city = e.currentTarget.value}
-      />
-    </FormGroup>
-    <Button>Search</Button>
-  </Form>
-  <Weather />
-</div>
+  <div className="text-center">
+    <Form>
+      <FormGroup>
+        <Label for="city">Search weather in:</Label>
+        <Input
+          type="text"
+          name="city"
+          id="city"
+          placeholder="City"
+          value={this.city}
+          onChange={e => (this.city = e.currentTarget.value)}
+          onKeyPress={e => {
+            if (e.key === 'Enter') {
+              this.search(e)
+            }
+          }}
+        />
+      </FormGroup>
+      <Button onClick={this.search}>Search</Button>
+    </Form>
+    <Weather />
+  </div>
 </div>
